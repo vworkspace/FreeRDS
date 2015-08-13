109aed8d22b8e343e8c5e0eb24606f6b78f658c1
Mike McDonald <Mike.McDonald@software.dell.com>
2015-08-13 10:39:38 -0400

    Modified branch in build script.

--- build-freerds.sh
+++ build-freerds.sh
@@ -33,8 +33,8 @@
 GIT_ROOT_DIR=~/git/vworkspace
 FREERDP_GIT=https://github.com/vworkspace/FreeRDP.git
 FREERDS_GIT=https://github.com/vworkspace/FreeRDS.git
-FREERDP_BRANCH=master
-FREERDS_BRANCH=master
+FREERDP_BRANCH=awakecoding
+FREERDS_BRANCH=awakecoding
 FREERDS_INSTALL_DIR=/opt/FreeRDS
 
 #
