af34a1715b61ccfd7175babe3a039fc746f76abc
Mike McDonald <Mike.McDonald@software.dell.com>
2016-08-24 11:59:48 -0400

    Fixed branches in build script.

--- build-freerds.sh
+++ build-freerds.sh
@@ -33,8 +33,8 @@
 GIT_ROOT_DIR=~/git/vworkspace
 FREERDP_GIT=https://github.com/vworkspace/FreeRDP.git
 FREERDS_GIT=https://github.com/vworkspace/FreeRDS.git
-FREERDP_BRANCH=master
-FREERDS_BRANCH=master
+FREERDP_BRANCH=freerds-v2.1
+FREERDS_BRANCH=v2.1
 FREERDS_INSTALL_DIR=/opt/FreeRDS
 
 #
